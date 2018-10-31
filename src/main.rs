b398df3b5cdd493100e714797e59e1b9d16ddb98
James Waples <jamwaffles@gmail.com>
2018-10-31 15:37:43 +0000

    Clean

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,16 @@
 #[macro_use]
 extern crate log;
 
-use futures;
-use futures::future::{ok as FutOk, Future};
-use futures::lazy;
+use futures::future::Future;
 use futures::{IntoFuture, Stream};
 use lapin_futures::channel::{
-    BasicConsumeOptions, BasicProperties, BasicPublishOptions, Channel, ExchangeDeclareOptions,
-    QueueBindOptions, QueueDeclareOptions,
+    BasicConsumeOptions, Channel, ExchangeDeclareOptions, QueueBindOptions, QueueDeclareOptions,
 };
 use lapin_futures::client::{Client, ConnectionOptions};
 use lapin_futures::consumer::Consumer;
 use lapin_futures::types::FieldTable;
-use serde_json;
 use std::io;
 use std::net::SocketAddr;
-use std::str;
 use std::sync::mpsc::channel;
 use std::sync::mpsc::Receiver;
 use std::sync::mpsc::Sender;
@@ -24,11 +19,6 @@ use std::thread::JoinHandle;
 use tokio;
 use tokio::net::TcpStream;
 use tokio::runtime::current_thread::block_on_all;
-use tokio::runtime::Runtime;
-
-struct Store {
-    event_stream: Stream<Item = (), Error = ()>,
-}
 
 fn connect(
     uri: SocketAddr,
@@ -124,51 +114,7 @@ fn create_consumer(
                 .map(move |stream| (channel, stream))
         })
         .map_err(|e| panic!("Hmm {:?}", e))
-    // .and_then(move |(channel, stream)| {
-    //     info!("got stream for consumer {}", 0);
-    //     // stream.for_each(move |message| {
-    //     //     let payload = str::from_utf8(&message.data).unwrap();
-    //     //     let data: Event<E> = serde_json::from_str(payload).unwrap();
-    //     //     trace!("Received message with ID {}: {}", data.id, payload);
-    //     //     handler(&data);
-    //     //     channel.basic_ack(message.delivery_tag, false)
-    //     // })
-    //     stream
-    // })
-    // .map(|_| ())
-    // .map_err(move |err| error!("got error in consumer {:?}", err))
 }
-
-// fn main() {
-//     pretty_env_logger::init();
-
-//     let uri: SocketAddr = "0.0.0.0:5672"
-//         .parse()
-//         .expect("Could not parse AMQP endpoint address");
-
-//     info!("Binding to {}", uri);
-
-//     let fut = connect(uri, "iris".to_string())
-//         .and_then(|(_client, channel)| create_consumer(channel, "queue.Name".to_string()))
-//         .and_then(|(channel, stream)| {
-//             stream
-//                 .for_each(move |message| {
-//                     println!("Event!");
-
-//                     channel.basic_ack(message.delivery_tag, false)
-//                 })
-//                 .map_err(|e| {
-//                     error!("{:?}", e);
-//                     ()
-//                 })
-//         });
-
-//     let handle = thread::spawn(|| {
-//         block_on_all(fut);
-//     });
-
-//     trace!("End program");
-// }
 
 struct Thingy {
     tx: Sender<i32>,
@@ -240,59 +186,6 @@ impl Thingy {
 
 fn main() {
     pretty_env_logger::init();
-
-    // Create a channel with a sending end (tx) and a receiving end (rx).
-    // let (tx, rx) = channel();
-
-    // // Spawn a new thread, and move the receiving end into the thread.
-    // let join_handle = thread::spawn(move || {
-    //     // Keep receiving in a loop, until tx is dropped!
-    //     while let Ok(n) = rx.recv() {
-    //         // Note: `recv()` always blocks
-    //         println!("Received {}", n);
-    //     }
-    // });
-
-    // let uri: SocketAddr = "0.0.0.0:5672"
-    //     .parse()
-    //     .expect("Could not parse AMQP endpoint address");
-
-    // info!("Binding to {}", uri);
-
-    // let fut = connect(uri, "iris".to_string())
-    //     .and_then(|(_client, channel)| create_consumer(channel, "queue.Name".to_string()))
-    //     .and_then(|(channel, stream)| {
-    //         stream
-    //             .for_each(move |message| {
-    //                 println!("Event!");
-
-    //                 tx.send(1i32).expect("Failed to send");
-
-    //                 channel.basic_ack(message.delivery_tag, false)
-    //             })
-    //             .map_err(|e| {
-    //                 error!("{:?}", e);
-    //                 ()
-    //             })
-    //     });
-
-    // trace!("Before sender spawn");
-
-    // let handle = thread::spawn(move || {
-    //     block_on_all(fut).expect("Block failed");
-    // });
-
-    // trace!("After sender spawn");
-
-    // // for i in 0..10 {
-    // //     tx.send(i).unwrap(); // Note: `send()` never blocks
-    // // }
-
-    // // // Drop `tx` so that `rx.recv()` returns an `Err(_)`.
-    // // drop(tx);
-
-    // // // Wait for the spawned thread to finish.
-    // handle.join().unwrap();
 
     let mut thing = Thingy::new();
 
